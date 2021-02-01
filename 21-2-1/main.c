8e440ab7a9e92150275a230acbc2d185d0d41db6
ganwang <74584251+ganwang-bit@users.noreply.github.com>
2021-02-01 20:48:18 +0800

    21-2-1

---
+++ 21-2-1/main.c
@@ -0,0 +1,53 @@
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//int main()
+//{
+//    printf("Hello world!\n");
+//    return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//    printf("are??)");
+//    return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//    printf("asdf\rasdaf");
+//    return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//    printf("asfdfas\n");
+//    printf("adsfadfsafs\n\b\r\r");
+//    printf("sfadfdsaf\n");
+//    printf("sdagfsdfasdfsadf\n");
+//    return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//    printf("sfsdas\t\t");
+//    printf("sadfasf");
+//    return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//    char *a="sfad";
+//    a[0]="3";
+//    return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//    int a=10;
+//    int c=20;
+//    const int *b=&a;
+//    b=&c;
+//    printf("%d",*b);
+//    return 0;
+//}
